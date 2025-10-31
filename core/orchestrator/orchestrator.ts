ca3e9e5223a89d56d52605769e963ada44bc4b24
daddy.loves.my.bunny@gmail.com <daddy.loves.my.bunny@gmail.com>
2025-10-31 22:45:06 +0100

    refactor: introduce Logger seam and console implementation

--- core/orchestrator/orchestrator.ts
+++ core/orchestrator/orchestrator.ts
@@ -1,5 +1,15 @@
+import { Logger, ConsoleLogger } from "../logger/logger.js";
+
 export class Orchestrator {
+  private logger: Logger;
+
+  constructor(logger: Logger = new ConsoleLogger()) {
+    this.logger = logger;
+  }
+
   run(): string {
-    return "Hello, here I am.";
+    const message = "Hello, here I am.";
+    this.logger.log(message);
+    return message;
   }
 }
